6276ab0384222eeb224c7b2c76a2cae838ccf6c7
Artur Kurek <artur.kurek@gmail.com>
2021-03-07 07:49:05 +0100

    disable postgresql

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ RUN:
 
 ```shell
 git clone git@github.com:arturro/mate-django-backend.git
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
 
 ...
 heroku run python manage.py migrate
--- ingredients/views.py
+++ ingredients/views.py
@@ -6,11 +6,11 @@ from . import models
 from . import serializers
 
 
-class CategoryViewset(viewsets.ModelViewSet):
+class CategoryVieSet(viewsets.ModelViewSet):
     queryset = models.Category.objects.all()
     serializer_class = serializers.CategorySerializer
 
 
-class IngredientViewset(viewsets.ModelViewSet):
+class IngredientViewSet(viewsets.ModelViewSet):
     queryset = models.Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
