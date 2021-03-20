6512372d49f79eea9e8193a935e184f0d3bfe3f2
Artur Kurek <a.kurek@il-pib.pl>
2021-03-20 10:37:26 +0100

    django-cors-headers, djangorestframework-simplejwt

--- matedjangobackend/settings.py
+++ matedjangobackend/settings.py
@@ -11,13 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-import django_heroku
-
 from pathlib import Path
+
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -61,7 +60,7 @@ ROOT_URLCONF = 'matedjangobackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,6 +72,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'matedjangobackend.wsgi.application'
 
@@ -145,6 +145,14 @@ CORS_ALLOWED_ORIGINS = [
     "https://mate-django-backend.herokuapp.com",
 ]
 
-"""
-Access to XMLHttpRequest at 'http://127.0.0.1:8000/api/v1/category/' from origin 'http://127.0.0.1:8001' has been blocked by CORS policy: Request header field access-control-allow-origin is not allowed by Access-Control-Allow-Headers in preflight response.
-"""
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
